0d4119b26caf109770374488f4f0c9f7aed34d04
showwin <showwin_kmc@yahoo.co.jp>
2024-03-24 15:00:36 +0900

    Random rooms

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,8 @@ mod map;
 pub use map::*;
 mod player;
 use player::*;
+mod rect;
+pub use rect::Rect;
 
 pub struct State {
     pub ecs: World,
@@ -42,11 +44,13 @@ fn main() -> rltk::BError {
     gs.ecs.register::<Renderable>();
     gs.ecs.register::<Player>();
 
-    gs.ecs.insert(new_map());
+    let (rooms, map) = new_map_rooms_and_corridors();
+    gs.ecs.insert(map);
+    let (player_x, player_y) = rooms[0].center();
 
     gs.ecs
         .create_entity()
-        .with(Position { x: 40, y: 25 })
+        .with(Position { x: player_x, y: player_y })
         .with(Renderable {
             glyph: rltk::to_cp437('@'),
             fg: RGB::named(rltk::YELLOW),
