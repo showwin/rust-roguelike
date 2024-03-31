ac9e688c6bcacbe12f65962789d5bd478b1c7189
showwin <showwin_kmc@yahoo.co.jp>
2024-03-31 13:09:04 +0900

    Done 2-4

--- src/map.rs
+++ src/map.rs
@@ -13,7 +13,8 @@ pub struct Map {
   pub rooms : Vec<Rect>,
   pub width: i32,
   pub height: i32,
-  pub revealed_tiles : Vec<bool>
+  pub revealed_tiles : Vec<bool>,
+  pub visible_tiles : Vec<bool>
 }
 
 impl Map {
@@ -56,7 +57,8 @@ impl Map {
       rooms: Vec::new(),
       width: 80,
       height: 50,
-      revealed_tiles: vec![false; 80*50]
+      revealed_tiles: vec![false; 80*50],
+      visible_tiles: vec![false; 80*50]
     };
 
     const MAX_ROOMS: i32 = 30;
@@ -117,14 +119,20 @@ pub fn draw_map(ecs: &World, ctx: &mut Rltk) {
   let mut x = 0;
   for (idx, tile) in map.tiles.iter().enumerate() {
     if map.revealed_tiles[idx] {
+      let glyph;
+      let mut fg;
       match tile {
         TileType::Floor => {
-            ctx.set(x, y, RGB::from_f32(0.5, 0.5, 0.5), RGB::from_f32(0., 0., 0.), rltk::to_cp437('.'));
+          glyph = rltk::to_cp437('.');
+          fg = RGB::from_f32(0.0, 0.5, 0.5);
         }
         TileType::Wall => {
-            ctx.set(x, y, RGB::from_f32(0.0, 1.0, 0.0), RGB::from_f32(0., 0., 0.), rltk::to_cp437('#'));
+          glyph = rltk::to_cp437('#');
+          fg = RGB::from_f32(0., 1.0, 0.);
         }
       }
+      if !map.visible_tiles[idx] { fg = fg.to_greyscale()}
+      ctx.set(x, y, fg, RGB::from_f32(0., 0., 0.), glyph);
     }
 
     // Move the coordinates
