dfe973465714cf61b218ba5598f04da11a208f08
showwin <showwin_kmc@yahoo.co.jp>
2024-04-16 22:36:59 +0900

    Add tooltip by mouseover

--- src/main.rs
+++ src/main.rs
@@ -103,9 +103,10 @@ impl GameState for State {
 
 fn main() -> rltk::BError {
     use rltk::RltkBuilder;
-    let context = RltkBuilder::simple80x50()
+    let mut context = RltkBuilder::simple80x50()
         .with_title("Roguelike Tutorial")
         .build()?;
+    context.with_post_scanlines(true);
 
     let mut gs = State {
         ecs: World::new()
