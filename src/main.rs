818f437d331f396e4f6419de041739a63d562b6a
showwin <showwin_kmc@yahoo.co.jp>
2024-05-09 22:18:27 +0900

    Pick up items

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use rltk::{GameState, Rltk, RGB, Point};
+use rltk::{GameState, Rltk, Point};
 use specs::prelude::*;
 
 mod components;
@@ -9,6 +9,8 @@ mod gamelog;
 pub use gamelog::GameLog;
 mod gui;
 pub use gui::draw_ui;
+mod inventory_system;
+pub use inventory_system::ItemCollectionSystem;
 mod map;
 pub use map::*;
 mod melee_combat_system;
@@ -21,6 +23,8 @@ mod player;
 use player::*;
 mod rect;
 pub use rect::Rect;
+mod spawner;
+pub use spawner::*;
 mod visibility_system;
 use visibility_system::VisibilitySystem;
 
@@ -47,6 +51,9 @@ impl State {
 
         let mut damage = DamageSystem{};
         damage.run_now(&self.ecs);
+
+        let mut pickup = ItemCollectionSystem{};
+        pickup.run_now(&self.ecs);
 
         self.ecs.maintain();
     }
@@ -122,49 +129,19 @@ fn main() -> rltk::BError {
     gs.ecs.register::<CombatStats>();
     gs.ecs.register::<WantsToMelee>();
     gs.ecs.register::<SufferDamage>();
+    gs.ecs.register::<Item>();
+    gs.ecs.register::<Potion>();
+    gs.ecs.register::<InBackpack>();
+    gs.ecs.register::<WantsToPickupItem>();
 
     let map : Map = Map::new_map_rooms_and_corridors();
     let (player_x, player_y) = map.rooms[0].center();
 
-    let player_entity = gs.ecs
-        .create_entity()
-        .with(Position { x: player_x, y: player_y })
-        .with(Renderable {
-            glyph: rltk::to_cp437('@'),
-            fg: RGB::named(rltk::YELLOW),
-            bg: RGB::named(rltk::BLACK),
-        })
-        .with(Player{})
-        .with(Viewshed{ visible_tiles : Vec::new(), range: 8, dirty: true })
-        .with(Name{name: "Player".to_string() })
-        .with(CombatStats{ max_hp: 30, hp: 30, defense: 2, power: 5 })
-        .build();
+    let player_entity = spawner::player(&mut gs.ecs, player_x, player_y);
 
-    let mut rng = rltk::RandomNumberGenerator::new();
-    for (i, room) in map.rooms.iter().skip(1).enumerate() {
-        let (x,y) = room.center();
-
-        let glyph : rltk::FontCharType;
-        let name : String;
-        let roll = rng.roll_dice(1, 2);
-        match roll {
-            1 => { glyph = rltk::to_cp437('g'); name = "Goblin".to_string(); }
-            _ => { glyph = rltk::to_cp437('o'); name = "Orc".to_string(); }
-        }
-
-        gs.ecs.create_entity()
-            .with(Position {x, y})
-            .with(Renderable {
-                glyph: glyph,
-                fg: RGB::named(rltk::RED),
-                bg: RGB::named(rltk::BLACK),
-            })
-            .with(Viewshed { visible_tiles: Vec::new(), range: 8, dirty: true})
-            .with(Monster {})
-            .with(Name { name: format!("{} #{}", &name, i) })
-            .with(CombatStats { max_hp: 16, hp: 16, defense: 1, power: 4 })
-            .with(BlocksTile{})
-            .build();
+    gs.ecs.insert(rltk::RandomNumberGenerator::new());
+    for room in map.rooms.iter().skip(1) {
+        spawner::spawn_room(&mut gs.ecs, room);
     }
 
     gs.ecs.insert(map);
